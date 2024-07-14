dae58b6291c4e9e2eb3757eedc7ec8d1025d2288
Malek Hamza <malek.hamza@gmx.de>
2024-07-14 22:53:05 +0200

    improve side nav

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,101 +1,15 @@
-import { cn } from "@/lib/utils";
-import {
-  Car,
-  CircleSlash,
-  Heater,
-  LayoutDashboard,
-  Menu,
-  ShieldCheck,
-  TreePalm,
-  Tv,
-  X,
-} from "lucide-react";
-import { ReactNode, useState } from "react";
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { Menu } from "lucide-react";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { SideNav } from "./SideNav";
 import { Button } from "./ui/button";
-
-type Link = {
-  icon: ReactNode;
-  text: string;
-  href: string;
-};
-
-const links: Link[] = [
-  {
-    icon: <LayoutDashboard />,
-    text: "Dashboard",
-    href: "/",
-  },
-  {
-    icon: <TreePalm />,
-    text: "Vacation",
-    href: "/category/vacation",
-  },
-  {
-    icon: <ShieldCheck />,
-    text: "Liability",
-    href: "/category/liability",
-  },
-  {
-    icon: <Heater />,
-    text: "Gas",
-    href: "/category/gas",
-  },
-  {
-    icon: <Car />,
-    text: "Car Insurance",
-    href: "/category/car-insurance",
-  },
-  {
-    icon: <Tv />,
-    text: "Public Broadcast",
-    href: "/category/public-broadcast",
-  },
-  {
-    icon: <CircleSlash />,
-    text: "No Category",
-    href: "/category/no-category",
-  },
-];
 
 export const Layout = () => {
   const [showSideNav, setShowSideNav] = useState(false);
 
   return (
     <div className="flex">
-      <div>
-        <div
-          className={cn(
-            "fixed bg-white md:static flex flex-shrink flex-col gap-3 p-5 h-screen z-10 border-r border-r-border shadow transition-all ease-in-out duration-300",
-            showSideNav ? "left-0" : "left-[-100%]"
-          )}
-        >
-          <div className="flex justify-between items-center">
-            <h1 className="font-bold">Savings</h1>
-            <Button
-              onClick={() => setShowSideNav(!showSideNav)}
-              variant="secondary"
-              className="md:hidden"
-            >
-              <X />
-            </Button>
-          </div>
-          {links.map((link) => (
-            <NavLink
-              className={({ isActive }) => cn(isActive ? "text-primary" : "")}
-              to={link.href}
-            >
-              <Button
-                className="flex justify-start items-center gap-1 font-bold w-full hover:text-primary"
-                variant={"ghost"}
-              >
-                {link.icon}
-                <p>{link.text}</p>
-              </Button>
-            </NavLink>
-          ))}
-        </div>
-      </div>
+      <SideNav showSideNav={showSideNav} setShowSideNav={setShowSideNav} />
       <div className="flex-grow overflow-y-auto h-screen lg:h-[90vh]">
         <div className="p-5">
           <Button
---
+++ frontend/src/components/SideNav.tsx
@@ -0,0 +1,73 @@
+import { cn } from "@/lib/utils";
+import { ArrowLeftRight, EuroIcon, LayoutDashboard, X } from "lucide-react";
+import { ReactNode } from "react";
+import { NavLink } from "react-router-dom";
+import { Button } from "./ui/button";
+
+type Link = {
+  icon: ReactNode;
+  text: string;
+  href: string;
+};
+
+const links: Link[] = [
+  {
+    icon: <LayoutDashboard />,
+    text: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: <ArrowLeftRight />,
+    text: "Transactions",
+    href: "/transactions",
+  },
+  {
+    icon: <EuroIcon />,
+    text: "Pockets",
+    href: "/pockets",
+  },
+];
+
+export const SideNav = ({
+  showSideNav,
+  setShowSideNav,
+}: {
+  showSideNav: boolean;
+  setShowSideNav: (value: boolean) => void;
+}) => {
+  return (
+    <div>
+      <div
+        className={cn(
+          "fixed bg-white min-w-[220px] md:static flex flex-shrink flex-col gap-3 p-5 h-screen z-10 border-r border-r-border shadow transition-all ease-in-out duration-300",
+          showSideNav ? "left-0" : "left-[-50%]"
+        )}
+      >
+        <div className="flex justify-between items-center">
+          <h1 className="font-bold">Savings</h1>
+          <Button
+            onClick={() => setShowSideNav(!showSideNav)}
+            variant="secondary"
+            className="md:hidden"
+          >
+            <X />
+          </Button>
+        </div>
+        {links.map((link) => (
+          <NavLink
+            className={({ isActive }) => cn(isActive ? "text-primary" : "")}
+            to={link.href}
+          >
+            <Button
+              className="flex justify-start items-center gap-1 font-bold w-full hover:text-primary"
+              variant={"ghost"}
+            >
+              {link.icon}
+              <p>{link.text}</p>
+            </Button>
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
