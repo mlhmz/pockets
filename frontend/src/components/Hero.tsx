f3b4e606ea30b345677249a4ac2b87decc0dd7b4
Malek Hamza <malek.hamza@gmx.de>
2025-01-21 00:27:55 +0100

    Add actual hero

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "react-oidc-context";
 import { Navigate, useLocation } from "react-router-dom";
 import { toast } from "sonner";
+import { Button } from "./ui/button";
 
 export const Hero = () => {
   const location = useLocation();
@@ -11,6 +12,11 @@ export const Hero = () => {
   if (isAuthenticated) {
     return <Navigate to="/app" replace state={{ from: location }} />;
   } else if (isAuthenticated != null && !isAuthenticated) {
-    return <><button onClick={() => signinRedirect()}>Login</button></>;
+    return <div className="w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center gap-3 border p-2 rounded-md shadow">
+        <p className="font-bold text-xl">Login with SSO</p>
+        <Button onClick={() => signinRedirect()}>Login</Button>
+      </div>
+    </div>;
   }
 }
