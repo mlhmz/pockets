a3cf31d5ae174b619a96aca01f08fa5c811acc46
Malek Hamza <malek.hamza@gmx.de>
2025-01-21 00:16:39 +0100

    Improve sidebar and oidc logout behaviour

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -5,6 +5,7 @@ import { SideNav } from "./SideNav";
 import { Button } from "./ui/button";
 import { useAuth } from "react-oidc-context";
 import { toast } from "sonner";
+import { SidebarProvider } from "./ui/sidebar";
 
 export const Layout = () => {
   const [showSideNav, setShowSideNav] = useState(false);
@@ -14,31 +15,33 @@ export const Layout = () => {
   error && toast.error(`An error occured while logging in: ${error?.message}`);
 
   if (isAuthenticated === undefined || isLoading) {
-		return (
-			<div className="flex flex-col justify-center items-center h-screen gap-3">
-				<span className="loading loading-spinner loading-lg"></span>
-				<p>Loading</p>
-			</div>
-		);
-	}
+    return (
+      <div className="flex flex-col justify-center items-center h-screen gap-3">
+        <span className="loading loading-spinner loading-lg"></span>
+        <p>Loading</p>
+      </div>
+    );
+  }
   if (!isAuthenticated) {
     return <Navigate to="/" replace state={{ from: location }} />
   }
   return (
     <div className="flex">
-      <SideNav showSideNav={showSideNav} setShowSideNav={setShowSideNav} />
-      <div className="flex-grow overflow-y-auto h-screen">
-        <div className="p-5">
-          <Button
-            onClick={() => setShowSideNav(!showSideNav)}
-            variant="secondary"
-            className="md:hidden"
-          >
-            <Menu />
-          </Button>
+      <SidebarProvider>
+        <SideNav showSideNav={showSideNav} setShowSideNav={setShowSideNav} />
+        <div className="flex-grow overflow-y-auto h-screen">
+          <div className="p-5">
+            <Button
+              onClick={() => setShowSideNav(!showSideNav)}
+              variant="secondary"
+              className="md:hidden"
+            >
+              <Menu />
+            </Button>
+          </div>
+          <Outlet />
         </div>
-        <Outlet />
-      </div>
+      </SidebarProvider>
     </div>
   );
 };
--- frontend/src/components/SideNav.tsx
+++ frontend/src/components/SideNav.tsx
@@ -1,9 +1,9 @@
-import { cn } from "@/lib/utils";
-import { ArrowLeftRight, ChevronDown, LayoutDashboard, User, Wallet, X } from "lucide-react";
+import { ArrowLeftRight, ChevronUp, LayoutDashboard, User2 } from "lucide-react";
 import { ReactNode } from "react";
-import { NavLink } from "react-router-dom";
-import { Button } from "./ui/button";
 import { useAuth } from "react-oidc-context";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
+import { Link, redirect } from "react-router-dom";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 type Link = {
   icon: ReactNode;
@@ -31,55 +31,52 @@ export const SideNav = ({
   showSideNav: boolean;
   setShowSideNav: (value: boolean) => void;
 }) => {
-  const { user } = useAuth();
-
+  const { user, signoutRedirect } = useAuth();
   return (
-    <div>
-      <div
-        className={cn(
-          "fixed bg-accent text-secondary-foreground min-w-[220px] md:static flex flex-shrink flex-col gap-3 p-5 h-screen z-10 border-r border-r-border shadow-sm transition-all ease-in-out duration-300",
-          showSideNav ? "left-0" : "left-[-80%]"
-        )}
-      >
-        <div className="flex justify-between items-center">
-          <Button
-            onClick={() => setShowSideNav(!showSideNav)}
-            variant="secondary"
-            className="md:hidden"
-          >
-            <X />
-          </Button>
-        </div>
-        <div className="flex flex-col items-center justify-center font-bold w-full py-5">
-          <Wallet size={52} className="text-secondary-foreground" />
-          <h1 className="text-xl">Savings</h1>
-        </div>
-        <div className="flex flex-col justify-between h-full">
-          <div>
-            {links.map((link) => (
-              <NavLink
-                className={({ isActive }) => cn(isActive ? "text-primary" : "")}
-                to={link.href}
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Savings Categorization</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {links.map((item) => (
+                <SidebarMenuItem key={item.text}>
+                  <SidebarMenuButton asChild>
+                    <Link to={item.href}>
+                      {item.icon}
+                      <span>{item.text}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> {user?.profile.preferred_username}
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
               >
-                <Button
-                  className="flex justify-start items-center gap-1 w-full hover:text-primary"
-                  variant={"ghost"}
-                >
-                  {link.icon}
-                  <p>{link.text}</p>
-                </Button>
-              </NavLink>
-            ))}
-          </div>
-          <Button variant={"outline"} className="flex justify-between w-full">
-            <div className="flex gap-2 items-center">
-              <User />
-              <p>{user?.profile.preferred_username}</p>
-            </div>
-            <ChevronDown />
-          </Button>
-        </div>
-      </div>
-    </div>
+                <DropdownMenuItem onClick={() => {
+                  signoutRedirect();
+                }}>
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
   );
 };
