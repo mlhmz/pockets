0f2b9844dbe436ead901b47d6080709b1977b2f4
Malek Hamza <malek.hamza@gmx.de>
2025-03-16 21:56:12 +0100

    fix: submit token on dkb csv upload

--- frontend/src/components/DkbCsvUpload.tsx
+++ frontend/src/components/DkbCsvUpload.tsx
@@ -2,11 +2,15 @@ import { Transaction } from "@/types/Transaction";
 import { useMutation } from "@tanstack/react-query";
 import { Loader, Upload } from "lucide-react";
 import { ChangeEvent, useRef, useState } from "react";
+import { useAuth } from "react-oidc-context";
 
-async function uploadCsv(file: File) {
+async function uploadCsv(file: File, token: string) {
   const result = await fetch("/api/v1/dkb", {
     method: "POST",
     body: file,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
   const data = await result.json();
   return data as Transaction[];
@@ -15,8 +19,9 @@ async function uploadCsv(file: File) {
 export const DkbCsvUpload = () => {
   const fileSelectRef = useRef<HTMLInputElement>(null);
   const [file, setFile] = useState<File | undefined>();
+  const { user } = useAuth();
   const { mutate } = useMutation({
-    mutationFn: (file: File) => uploadCsv(file),
+    mutationFn: (file: File) => uploadCsv(file, user?.access_token!),
     onSuccess: () => setFile(undefined),
   });
 
