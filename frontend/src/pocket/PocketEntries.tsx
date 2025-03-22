bb6e31b8f3e27af8ed20aa4bbe12bbf23656ef59
Malek Hamza <malek.hamza@gmx.de>
2025-03-23 00:32:55 +0100

    fix: dashboard card layouting responsibility

--- frontend/src/pocket/PocketEntries.tsx
+++ frontend/src/pocket/PocketEntries.tsx
@@ -4,7 +4,7 @@ import { Pockets } from "@/types/Pocket";
 import { Link } from "react-router-dom";
 
 export const PocketEntries = ({ data }: { data?: Pockets }) => (
-  <div className="grid grid-cols-3 gap-3">
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
     {data?.map((entry) => (
       <Link key={entry.uuid} to={"/app/transactions/" + entry?.uuid}>
         <Card key={entry.uuid} className="hover:bg-muted transition-all cursor-pointer">
