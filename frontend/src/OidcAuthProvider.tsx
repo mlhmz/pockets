10448106f078c8055c2c39edec0f3b96ff3d6d54
Malek Hamza <malek.hamza@gmx.de>
2025-03-23 00:30:21 +0100

    feat: Better loader for AuthProvider

--- frontend/src/OidcAuthProvider.tsx
+++ frontend/src/OidcAuthProvider.tsx
@@ -1,28 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
+import { LoaderCircle } from "lucide-react";
 import React from "react";
 import { AuthProvider } from "react-oidc-context";
 
 interface OidcConfig {
-    authority: string;
-    client_id: string;
-    redirect_uri: string;
+  authority: string;
+  client_id: string;
+  redirect_uri: string;
 }
 
 async function getOidcConfig() {
-    const response = await fetch("/oidc-config");
-    const data = await response.json();
-    return data as OidcConfig;
+  const response = await fetch("/oidc-config");
+  const data = await response.json();
+  return data as OidcConfig;
 }
 
 export const OidcAuthProvider = ({ children }: { children: React.ReactNode }) => {
-    const {data, isLoading} = useQuery({
-        queryKey: ["oidc-config"],
-        queryFn: getOidcConfig,
-        retry: 3,
-    });
+  const { data, isLoading } = useQuery({
+    queryKey: ["oidc-config"],
+    queryFn: getOidcConfig,
+    retry: 3,
+  });
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-    return <AuthProvider {...data}>{children}</AuthProvider>;
+  if (isLoading) {
+    return <div className="flex flex-col justify-center items-center h-screen w-screen gap-3">
+      <LoaderCircle size={32} className="animate-spin ease-in-out" />
+      <p>Authenticating...</p>
+    </div>;
+  }
+  return <AuthProvider {...data}>{children}</AuthProvider>;
 }
